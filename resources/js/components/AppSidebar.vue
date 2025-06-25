826b6768eb00a9b63dbfd1595f4a2701f2baf03a
DouradoCtrl <gdouradoestudante@gmail.com>
2025-06-25 17:49:21 -0300

    news navlinks

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -2,30 +2,36 @@
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
 import NavUser from '@/components/NavUser.vue';
-import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem} from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-vue-next';
+import { BookOpen, Folder, LayoutGrid, Mail, Users, UserCog} from 'lucide-vue-next';
 import AppLogo from './AppLogo.vue';
 
 const mainNavItems: NavItem[] = [
     {
-        title: 'Dashboard',
+        title: 'Painel',
         href: '/dashboard',
         icon: LayoutGrid,
     },
+    {
+        title: 'Clientes',
+        href: '/clientes',
+        icon: UserCog,
+    },
+    {
+        title: 'Mensagens',
+        href: '/mensagens',
+        icon: Mail,
+    },
 ];
+
 
 const footerNavItems: NavItem[] = [
     {
-        title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
+        title: 'Users',
+        href: '/users',
+        icon: Users,
     },
 ];
 </script>

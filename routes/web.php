26feff81cda3674ea6a517e8048f48899fdc8a41
DouradoCtrl <gdouradoestudante@gmail.com>
2025-06-26 16:31:51 -0300

    Rota index ClienteController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\ClienteController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -10,6 +11,10 @@ Route::get('/', function () {
 Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('clientes', [ClienteController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('clientes');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';

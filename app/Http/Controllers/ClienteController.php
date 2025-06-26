452ea7ce8f4e3eff6c008d95bf73d5dae747bd2a
DouradoCtrl <gdouradoestudante@gmail.com>
2025-06-26 17:53:29 -0300

    Envio de request

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -8,7 +8,7 @@ use Inertia\Inertia;
 
 class ClienteController extends Controller
 {
-    public function index() {
+    public function index(Request $request) {
         $clientes = Cliente::paginate(15);
 
         return Inertia::render('clientes/Clientes', ['clientes' => $clientes]);
